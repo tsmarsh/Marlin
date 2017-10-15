f0d69d6a4eb13a8641f639b7d29409ef8c7b397b
Tom Marsh <ts.marsh@gmail.com>
2017-10-14 23:51:54 -0400

    Updating PIDs

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -356,9 +356,13 @@
 //  #define  DEFAULT_Kd 114
 
   // MakerGear
-  #define  DEFAULT_Kp 7.0
-  #define  DEFAULT_Ki 0.1
-  #define  DEFAULT_Kd 12
+//  #define  DEFAULT_Kp 7.0
+//  #define  DEFAULT_Ki 0.1
+//  #define  DEFAULT_Kd 12
+
+#define  DEFAULT_Kp 38.50
+#define  DEFAULT_Ki 4.35
+#define  DEFAULT_Kd 85.15
 
   // Mendel Parts V9 on 12V
   //#define  DEFAULT_Kp 63.0
@@ -395,9 +399,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define  DEFAULT_bedKp 10.00
-  #define  DEFAULT_bedKi .023
-  #define  DEFAULT_bedKd 305.4
+#define  DEFAULT_bedKp 158.87
+#define  DEFAULT_bedKi 29.83
+#define  DEFAULT_bedKd 211.49
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
